c5661170c1b696ac4d8504b2c95b4ea9ebe7c7f3
jihyuno301 <joh755807@gmail.com>
2019-02-22 23:36:50 +0900

    한수

--- src/io/Q1065.java
+++ src/io/Q1065.java
@@ -18,12 +18,17 @@ public class Q1065 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
-		//bw.write(hansoo(N)+"\n");
+		bw.write(hansoo(N)+"\n");
+		bw.flush();
+		bw.close();
 		
+	}
+	
+	public static int hansoo(int N) {
 		int count = 0;
 		if(N<100) { //한두자리수는 한수
 			count = N;
-			bw.write(count+"\n"); 
+			return count;
 		}
 		//100이상인 경우
 		count += 99; //1부터99까지
@@ -31,82 +36,31 @@ public class Q1065 {
 			int d=0, temp;
 			int bef=0, now=0;
 			int subCount=0;
-			while(i!=0) {
-				now = i%10;
+			int r = i;
+			while(r!=0) {
+				now = r%10;
 				if(subCount==0) {
 					bef = now;
-					subCount+=1;
-					i = i/10;
-					bw.write("subCount=0\n");
+					subCount++;
+					r = r/10;
 				}
 				else if(subCount==1) {
-					d = Math.abs(bef-now); //공차 구하기
+					d = bef-now; //공차 구하기
 					bef = now;
-					i = i/10;
-					subCount+=1;
-					bw.write("subCount=1\n");
+					subCount++;
+					r = r/10;
 				}
 				else {
-					temp = Math.abs(bef-now);
+					temp = bef-now;
 					if(d != temp) break;
 					bef = now;
-					i = i/10;
-					bw.write("subCount>=2\n");
+					r = r/10;
 					//더이상 subCount할 필요x
 				}
-				
 			}
-			if(i==0) count++;
-			
+			if(r==0) count++;
 		}
-		
-		bw.write(count+"\n");
-		
-		bw.flush();
-		bw.close();
-	}
-	
-	public static int hansoo(int num) {
-		int count = 0;
-		if(num<100) { //한두자리수는 한수
-			count = num;
-			return count; 
-		}
-		//100이상인 경우
-		count += 99; //1부터99까지
-		for(int i=100; i<=num; i++) {
-			int d=0, temp;
-			int bef=0, now=0;
-			int subCount=0;
-			while(i!=0) {
-				now = i%10;
-				if(subCount==0) {
-					bef = now;
-					i = i/10;
-					subCount++;
-					continue;
-				}
-				else if(subCount==1) {
-					d = Math.abs(bef-now); //공차 구하기
-					bef = now;
-					i = i/10;
-					subCount++;
-					continue;
-				}
-				else {
-					temp = Math.abs(bef-now);
-					if(d != temp) break;
-					bef = now;
-					i = i/10;
-					//더이상 subCount할 필요x
-				}
-				
-			}
-			if(i==0) count++;
-			
-		}
-		
 		return count;
 	}
-
+	
 }
