8c8e379854aa42eb4b40beab22353fed6c7c291d
jihyuno301 <joh755807@gmail.com>
2019-03-14 22:40:26 +0900

    짝수홀수분리하기

--- src/SWproject/P01.java
+++ src/SWproject/P01.java
@@ -8,19 +8,15 @@ import java.util.Scanner;
 
 
 public class P01 {
+	
 	public static void swap(int[] arr, int i, int j) {
 		int temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
 	}
 	
-	public static void sort(int[] arr) {
-		int start = -1; //짝수 마지막값
-		for(int i=0; i<arr.length; i++) {
-			if(arr[i]%2==0) swap(arr,++start, i);
-		}
-	}
 	public static void main(String[] args) {
+		
 		Scanner input = new Scanner(System.in);
 		System.out.print("입력받을 숫자의 개수 : ");
 		int N = input.nextInt();
@@ -28,6 +24,12 @@ public class P01 {
 		for(int i=0; i<N; i++) {
 			arr[i] = input.nextInt();
 		}
+		
+		int start = -1; //짝수 마지막값
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i]%2==0) swap(arr,++start, i);
+		}
+		
 		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<arr.length; i++) {
 			sb.append(arr[i]+" ");
