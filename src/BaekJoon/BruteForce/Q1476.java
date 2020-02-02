b5bbf6370930701e90c6f97cb68e19f566d988aa
jihyuno301 <joh755807@gmail.com>
2020-02-02 18:12:31 +0900

    날짜 계산

--- src/BaekJoon/BruteForce/Q1476.java
+++ src/BaekJoon/BruteForce/Q1476.java
@@ -2,30 +2,32 @@
 Title : 날짜 계산
 URL : https://www.acmicpc.net/problem/1476
 
-- 시간초과 오류
 */
 package BaekJoon.BruteForce;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Q1476 {
+
 	static int solution(int E, int S, int M) {
 		int year = 1;
 		while(true) {
-			if((year % 15 == E) && (year % 28 == S) && (year % 19 == M)) return year;
+			// if((year % 15 == E) && (year % 28 == S) && (year % 19 == M)) return year; -> 시간 초과의 원인
+			if((year-E)%15 == 0 && (year-S)%28 == 0 && (year-M)%19 == 0) {
+				System.out.println(year);
+				break;
+			}
 			year++;
 		}
+		return year;
 	}
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int E = Integer.parseInt(st.nextToken());
-		int S = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
 
-		System.out.println(solution(E,S,M));
+	public static void main(String[] args)  {
+		Scanner sc = new Scanner(System.in);
+		int E = sc.nextInt();
+		int S = sc.nextInt();
+		int M = sc.nextInt();
+
+		solution(E,S,M);
 	}
 }
